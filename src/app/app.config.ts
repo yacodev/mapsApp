c3500d1a6e3a0290ee2dd51da155cc7cbb87d1df
yacodev <cyaco33@gmail.com>
2025-03-12 14:42:31 -0500

    adde has location eastrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // hash location strategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+  ],
 };
